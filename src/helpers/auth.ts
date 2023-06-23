e2aec5a670a0f714cb0e24d01cca5b11c6a1ca26
Dumitru Merlusca <dm@visitor-analytics.io>
2023-06-23 16:19:13 +0300

    Updated auth helpers

--- src/helpers/auth.ts
+++ src/helpers/auth.ts
@@ -1,19 +1,24 @@
 import jwt from "jsonwebtoken";
 import { v4 as uuidv4 } from "uuid";
 
+type UserData = { isAdmin?: boolean };
 declare global {
-  var getAuthToken: (userId?: string) => string;
-  var getAuthHeader: (userId?: string) => string;
+  var getAuthToken: (userId?: string, data?: UserData) => string;
+  var getAuthHeader: (userId?: string, data?: UserData) => string;
 }
 
-global.getAuthToken = (userId?: string) => {
+global.getAuthToken = (userId?: string, data?: UserData) => {
   const defaultId = uuidv4();
-  return jwt.sign({ id: userId || defaultId }, process.env.JWT_SECRET!, {
-    expiresIn: "1h",
-  });
+  return jwt.sign(
+    { id: userId || defaultId, ...data },
+    process.env.JWT_SECRET!,
+    {
+      expiresIn: "1h",
+    }
+  );
 };
-global.getAuthHeader = (userId?: string) => {
-  const token = global.getAuthToken(userId);
+global.getAuthHeader = (userId?: string, data?: UserData) => {
+  const token = global.getAuthToken(userId, data);
   return `Bearer ${token}`;
 };
 
